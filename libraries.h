46352bc9b07f78ba39a8d9e5f5d52891ac8ad000
dzgeorgy <dzgeorgy@gmail.com>
2020-10-26 23:56:51 +0300

    [Requirement]Replaced include guards in favor of pragma once
    Signed-off-by: dzgeorgy <dzgeorgy@gmail.com>

--- libraries.h
+++ libraries.h
@@ -1,8 +1,1 @@
-//
-// Created by Georgy Dzutsev on 23.10.2020.
-//
-
-#ifndef LAB1__LIBRARIES_H_
-#define LAB1__LIBRARIES_H_
-
-#endif //LAB1__LIBRARIES_H_
+#pragma once
--- prototypes.h
+++ prototypes.h
@@ -1,8 +1,1 @@
-//
-// Created by Georgy Dzutsev on 23.10.2020.
-//
-
-#ifndef LAB1__PROTOTYPES_H_
-#define LAB1__PROTOTYPES_H_
-
-#endif //LAB1__PROTOTYPES_H_
+#pragma once
