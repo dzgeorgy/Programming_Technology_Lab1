eb801d2b4d65e833c93fe320228e7470778403e8
dzgeorgy <dzgeorgy@gmail.com>
2020-10-27 14:23:58 +0300

    Added main menu
    Signed-off-by: dzgeorgy <dzgeorgy@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
-#include <iostream>
+#include "prototypes.h"
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+int main()
+{
+	while (true)
+	{
+		if (!main_menu())
+			return 0;
+	}
+}
+
+bool main_menu()
+{
+	std::printf("Choose an option:\n"
+				"1: Task1\n"
+				"2: Task2\n"
+				"3: Task3\n"
+				"4: Exit\n");
+	try
+	{
+		auto action = read_int();
+		switch (action)
+		{
+		case 1:
+			printf("Option 1\n");
+			return true;
+		case 2:
+			printf("Option 2\n");
+			return true;
+		case 3:
+			printf("Option 3\n");
+			return true;
+		case 4:
+			return false;
+		default:
+			throw std::invalid_argument("");
+		}
+	}
+	catch (const std::invalid_argument& exception)
+	{
+		printf("You entered incorrect value! Please try again.\n\n\n");
+		return true;
+	}
 }
--- prototypes.h
+++ prototypes.h
@@ -1,3 +1,21 @@
 #pragma once
 
 #include "libraries.h"
+
+/**
+ * Prints out main menu and handles user's choice of menu items
+ * @return True if application should continue work and false otherwise.
+ */
+bool main_menu();
+
+/**
+ * Tries to read integer value from console, throws an exception if not succeed
+ * @return Integer value
+ * @throws std::invalid_argument
+ */
+int read_int()
+{
+	char buffer[10];
+	std::scanf("%s", buffer);
+	return std::stoi(buffer, nullptr, 10);
+}
